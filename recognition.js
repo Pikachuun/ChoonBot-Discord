ae4611feef5315170109fd60ebb1bc04041df980
Pikachuun <pikachuun@gmail.com>
2016-04-23 13:58:16 -0400

    whoops

--- recognition.js
+++ recognition.js
@@ -337,7 +337,7 @@ exports.commands = {
 			let praise = ["It is certain", "It is decidedly so", "Without a doubt", "Yes definitely", "You may rely on it", "As I see it, yes", "Most likely", "Outlook good", "Yes", "Signs point to yes", "Reply hazy try again", "Ask again later", "Better not tell you now", "Cannot predict now", "Concentrate and ask again ", "Don't count on it", "My reply is no", "My sources say no", "Outlook not so good", "Very doubtful", "no.", "START", "A", "B", "UP", "DOWN", "LEFT", "RIGHT", "SELECT"];
 			choonbot.sendMessage(message.channel, "The helix fossil says: **" + praise[Math.floor(Math.random()*praise.length)] + "**");
 		}
-	}
+	},
 	pi: {
 		command: function (message, args) {
 			commands.pie.command(message, args, true);
